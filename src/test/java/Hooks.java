8a85105ac9e7a1078e9a82ff7836814a51e22023
Lehi - ASUS Vivo <lehirolim@gmail.com>
2024-10-22 15:17:32 -0300

    Aqui realmente funciona

--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Hooks {
+public class Hooks
+{
+    // Variáveis Necessárias
+    public static WebDriver driver;
+    public static ChromeOptions options;
+
+    @BeforeEach
+    public void abrir()
+    {
+        WebDriverManager.chromedriver().setup(); // Configurando o driver do navegador
+        options = new ChromeOptions();
+        options.addArguments(("--disable-crash-reporter"));
+        options.addArguments(("--disable-extensions"));
+        options.addArguments(("--disable-dev-shm"));
+        options.addArguments(("--disable-notifications"));
+        driver = new ChromeDriver(options);   // abrindo o navegador
+        //driver.get("https://google.com.br/");
+    }
+
+    @AfterEach
+    public void fechar()
+    {
+        driver.quit();
+        driver = null;
+    }
 }
--- src/test/java/Primeira.java
+++ src/test/java/Primeira.java
@@ -1,9 +1,31 @@
-package lehi;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Primeira
+import java.util.concurrent.TimeUnit;
+
+public class Primeira extends Hooks
 {
-    public static void main(String[] args)
+//    By pathImputpesquisa = By.xpath("//textarea[@id='pass']");
+
+    @Test
+    @DisplayName("Cenario: Primeiro teste do Lehinho.")
+    public void teste01() throws InterruptedException
     {
-        WebDriverManager.chromedriver().setup();
+        // TESTE
+        driver.get("http://bug.dev.hill/signin");
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).sendKeys("lehi@local.dev");
+        driver.findElement(By.id("pass")).click();
+        driver.findElement(By.id("pass")).sendKeys("qwe123");
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(2000);
+        By toast = By.cssSelector("#toast_notification div:nth-child(1) div.toast-body");
+        String msgTela = driver.findElement(toast).getText();
+        Assertions.assertEquals("Login com sucesso!", msgTela);
     }
+
 }
