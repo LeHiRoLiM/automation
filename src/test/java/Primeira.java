93568b40d895ab3dfd2467f9c42290b2a12ebb11
Lehi - ASUS Vivo <lehirolim@gmail.com>
2024-10-22 16:33:06 -0300

    Teste Final

--- src/test/java/Primeira.java
+++ src/test/java/Primeira.java
@@ -12,7 +12,7 @@ public class Primeira extends Hooks
 //    By pathImputpesquisa = By.xpath("//textarea[@id='pass']");
 
     @Test
-    @DisplayName("Cenario: Primeiro teste do Lehinho.")
+    @DisplayName("Cenário: CENÁRIO LOGIN CORRETO 01")
     public void teste01() throws InterruptedException
     {
         // TESTE
@@ -26,6 +26,96 @@ public class Primeira extends Hooks
         By toast = By.cssSelector("#toast_notification div:nth-child(1) div.toast-body");
         String msgTela = driver.findElement(toast).getText();
         Assertions.assertEquals("Login com sucesso!", msgTela);
+        Thread.sleep(1000);
+        System.out.println(msgTela);
     }
 
+   @Test
+   @DisplayName("Cenário: SENHA ERRADA 02")
+    public void teste02() throws InterruptedException
+   {
+       // TESTE
+       driver.get("http://bug.dev.hill/signin");
+       driver.findElement(By.id("email")).click();
+       driver.findElement(By.id("email")).sendKeys("lehi@local.dev");
+       driver.findElement(By.id("pass")).click();
+       driver.findElement(By.id("pass")).sendKeys("qwe321");
+       driver.findElement(By.id("login")).click();
+       Thread.sleep(2000);
+       driver.findElement(By.id("toast_notification"));
+       Thread.sleep(1000);
+       System.out.println("Incorrect Password");
+   }
+
+    @Test
+    @DisplayName("Cenário: EMAIL ERRADO 03")
+    public void teste03() throws InterruptedException
+    {
+        driver.get("http://bug.dev.hill/signin");
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).sendKeys("lehi@local.devinho");
+        driver.findElement(By.id("pass")).click();
+        driver.findElement(By.id("pass")).sendKeys("qwe321");
+        driver.findElement(By.id("login")).click();
+        // Thread.sleep(2000);
+        driver.findElement(By.id("toast_notification"));
+        Thread.sleep(1000);
+        System.out.println("User not foud");
+    }
+
+    @Test
+    @DisplayName("Cenário: EMAIL E SENHA ERRADO 04")
+    public void teste04() throws InterruptedException {
+        driver.get("http://bug.dev.hill/signin");
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).sendKeys("lehi@local.devinho");
+        driver.findElement(By.id("pass")).click();
+        driver.findElement(By.id("pass")).sendKeys("qwe3210");
+        driver.findElement(By.id("login")).click();
+        driver.findElement(By.id("toast_notification"));
+        Thread.sleep(1000);
+        System.out.println("Incorrect Password");
+
+    }
+    @Test
+    @DisplayName("Cenário: EMAIL CORRETO E SENHA VAZIO 05")
+    public void teste05() throws InterruptedException {
+        driver.get("http://bug.dev.hill/signin");
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).sendKeys("lehi@local.dev");
+        driver.findElement(By.id("pass")).click();
+        driver.findElement(By.id("pass")).sendKeys("");
+        driver.findElement(By.id("login")).click();
+        driver.findElement(By.id("toast_notification"));
+        Thread.sleep(1000);
+        System.out.println("Fill in Password");
+    }
+
+    @Test
+    @DisplayName("Cenário: EMAIL VAZIO E SENHA CORRETA 06")
+    public void teste06() throws InterruptedException {
+        driver.get("http://bug.dev.hill/signin");
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).sendKeys("");
+        driver.findElement(By.id("pass")).click();
+        driver.findElement(By.id("pass")).sendKeys("qwe123");
+        driver.findElement(By.id("login")).click();
+        driver.findElement(By.id("toast_notification"));
+        Thread.sleep(1000);
+        System.out.println("Fill in email");
+    }
+
+    @Test
+    @DisplayName("Cenário: EMAIL E SENHA VAZIO 07")
+    public void teste07() throws InterruptedException {
+        driver.get("http://bug.dev.hill/signin");
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).sendKeys("");
+        driver.findElement(By.id("pass")).click();
+        driver.findElement(By.id("pass")).sendKeys("");
+        driver.findElement(By.id("login")).click();
+        driver.findElement(By.id("toast_notification"));
+        Thread.sleep(1000);
+        System.out.println("Fill in email");  // por causa da ordem de precedência
+    }
 }
